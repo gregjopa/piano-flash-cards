a5a6a1efa262cf8d9b342a16579f498efc66109d
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-01-01 19:47:19 -0600

    chore: hide subtitle on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
       <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight">
         Piano Flash Cards
       </h1>
-      <p className="mt-6 text-lg text-slate-600">
+      <p className="mt-6 text-lg text-slate-600 hidden sm:block">
         Practice memorizing notes with these treble and bass clef{" "}
         <span className="whitespace-nowrap">flash cards</span>.
       </p>
