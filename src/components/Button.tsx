e22a7a344a2099b5a32fd7e67e8d6513b7407ade
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-01-05 10:31:52 -0600

    chore: update button focus style to match select form component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className="w-full bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+      className="w-full bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800 focus:border focus:border-blue-300 focus:outline-none focus:ring-4 focus:ring-blue-300"
       onClick={onClick}
       autoFocus={autoFocus}
     >
