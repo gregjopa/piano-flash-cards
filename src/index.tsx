7cc9bf3b6a4fb70df375435b3def3e2023dfdc27
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-01-12 16:48:35 -0600

    feat: integrate web vitals with google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { onCLS, onFID, onLCP, Metric } from "web-vitals";
+
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,7 +14,30 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// https://github.com/GoogleChrome/web-vitals#using-gtagjs-universal-analytics
+function sendToGoogleAnalytics({ name, delta, id }: Metric) {
+  gtag("event", name, {
+    event_category: "Web Vitals",
+    // The `id` value will be unique to the current page load. When sending
+    // multiple values from the same page (e.g. for CLS), Google Analytics can
+    // compute a total by grouping on this ID (note: requires `eventLabel` to
+    // be a dimension in your report).
+    event_label: id,
+    // Google Analytics metrics must be integers, so the value is rounded.
+    // For CLS the value is first multiplied by 1000 for greater precision
+    // (note: increase the multiplier for greater precision if needed).
+    value: Math.round(name === "CLS" ? delta * 1000 : delta),
+    // Use a non-interaction event to avoid affecting bounce rate.
+    non_interaction: true,
+
+    // OPTIONAL: any additional attribution params here.
+    // See: https://web.dev/debug-performance-in-the-field/
+    // dimension1: '...',
+    // dimension2: '...',
+    // ...
+  });
+}
+
+onCLS(sendToGoogleAnalytics);
+onFID(sendToGoogleAnalytics);
+onLCP(sendToGoogleAnalytics);
